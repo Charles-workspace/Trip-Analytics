23cfd415e4fab4d02bf4a3b3fce5acff8e9a4749
Charles santhakumar <charlessanthakumarxyz@gmail.com>
2025-12-11 15:23:19 +0200

    Feature/weather wrapper fix (#66)
    * weather_function_update
    
    * weather_function_update
    
    * main pipeline args fix
    
    * main pipeline args fix

--- src/trip_pipeline/sprocs/load_weather_sproc.py
+++ src/trip_pipeline/sprocs/load_weather_sproc.py
@@ -4,14 +4,13 @@ from snowflake.snowpark import Session
 from trip_pipeline.ingestion.load_weather_from_stage import load_weather_from_stage
 
 
-def run(session: Session, start_date: str, end_date: str) -> str:
+def run(session: Session) -> str:
     """
     Stored Procedure: Loads weather CSV data from Snowflake landing stage
     into the landing weather table.
     """
     try:
-        load_weather_from_stage(session, start_date, end_date)
-        return (f"SP_LOAD_WEATHER_FROM_STAGE: Weather data loaded successfully "
-                f"for {start_date} to {end_date}.")
+        load_weather_from_stage(session)
+        return (f"SP_LOAD_WEATHER_FROM_STAGE: Weather data loaded successfully ")
     except Exception as e:
         return f"SP_LOAD_WEATHER_FROM_STAGE failed: {e}"
